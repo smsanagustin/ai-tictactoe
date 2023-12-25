9066119b96180b0ed24da708904d6ba58b2458f0
Sophia San Agustin <smsanagustin@up.edu.ph>
2023-12-25 15:56:29 +0800

    deleted file

--- hi.py
+++
@@ -1,8 +0,0 @@
-original_list = [1,2,3,4, ""]
-list_copy = []
-for i in range(len(original_list)):
-    list_copy.append(original_list[i])
-    
-original_list[0] = 5
-print(original_list)
-print(list_copy)
